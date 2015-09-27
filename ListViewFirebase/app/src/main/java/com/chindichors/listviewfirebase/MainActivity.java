3b91ffaf90c7bb3d364f8d3d438c2267f2b7773b
ajit <ajeet001@gmail.com>
2015-09-26 23:00:08 -0700

    Youtube to YouTube
    description to Descriptiom

--- ListViewFirebase/app/src/main/java/com/chindichors/listviewfirebase/MainActivity.java
+++ ListViewFirebase/app/src/main/java/com/chindichors/listviewfirebase/MainActivity.java
@@ -25,10 +25,10 @@ public class MainActivity extends Activity {
                 execute("http://img.youtube.com/vi/7GqClqvlObY/default.jpg");
 
         TextView imageTitle = (TextView)findViewById(R.id.title);
-        imageTitle.setText("Youtube Video Title");
+        imageTitle.setText("YouTube Video Title");
 
         TextView imageDesc = (TextView)findViewById(R.id.desc);
-        imageDesc.setText("Youtube Video description");
+        imageDesc.setText("YouTube Video Description");
 
 
     }
